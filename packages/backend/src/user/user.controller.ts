016d3238e313e65ea502cad9517d4ae04d758d24
mrzombit <54615665+mrzombit@users.noreply.github.com>
2022-12-09 00:45:04 +0700

    GET PUT DEL - users 100%

--- packages/backend/src/user/user.controller.ts
+++ packages/backend/src/user/user.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import { ValidateObjectId } from './shared/pipes/validate-object-id.pipes';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UserService } from './user.service';
+import { Controller, Get, Res, HttpStatus, Param, NotFoundException, Post, Body, Put, Query, Delete } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor(private userService: UserService){ }
+
+    @Post('/post')
+    async addUser(@Res() res, @Body() createUserDTO: CreateUserDTO){
+        const newUser = await this.userService.addUser(createUserDTO);
+        return res.status(HttpStatus.OK).json({
+            message: 'User has been submitted successfully!',
+            post: newUser,
+        });
+    }
+    @Get('post/:userID') //"../user/post/1"
+    async getUser(@Res() res, @Param('userID', new ValidateObjectId()) userID) {
+    const user = await this.userService.getUser(userID);
+    if (!user) {
+        throw new NotFoundException('User does not exist!');
+    }
+    return res.status(HttpStatus.OK).json(user);
+    }
+
+    @Get('users') //"../user/users"
+    async getUsers(@Res() res) {
+    const users = await this.userService.getUsers();
+    return res.status(HttpStatus.OK).json(users);
+    }
+
+    @Put('/edit')
+    async editUser(
+      @Res() res,
+      @Query('userID', new ValidateObjectId()) userID,
+      @Body() createUserDTO: CreateUserDTO,
+    ) {
+      const editedUser = await this.userService.editUser(userID, createUserDTO);
+      if (!editedUser) {
+          throw new NotFoundException('User does not exist!');
+      }
+      return res.status(HttpStatus.OK).json({
+        message: 'User has been successfully updated',
+        user: editedUser,
+      });
+    }
+
+    @Delete('/delete')
+    async deleteUser(@Res() res, @Query('userID', new ValidateObjectId()) userID) {
+      const deletedUser = await this.userService.deleteUser(userID);
+      if (!deletedUser) {
+          throw new NotFoundException('User does not exist!');
+      }
+      return res.status(HttpStatus.OK).json({
+        message: 'User has been deleted!',
+        user: deletedUser,
+      });
+    }
+}
--- packages/backend/src/user/user.service.ts
+++ packages/backend/src/user/user.service.ts
@@ -1,4 +1,38 @@
+import { CreateUserDTO } from './dto/create-user.dto';
+import { User } from './interfaces/user.interface';
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectModel('User') private readonly userModel: Model<User>) { }
+
+    async addUser(createUserDTO: CreateUserDTO): Promise<User> {
+        const newUser = await new this.userModel(createUserDTO);
+        return newUser.save();
+      }  
+        
+      async getUser(userID): Promise<User> {
+        const user = await this.userModel
+          .findById(userID)
+          .exec();
+        return user;
+      }
+        
+      async getUsers(): Promise<User[]> {
+        const users = await this.userModel.find().exec();
+        return users;
+      }
+    
+      async editUser(userID, createUserDTO: CreateUserDTO): Promise<User> {
+        const editedUser = await this.userModel
+          .findByIdAndUpdate(userID, createUserDTO, { new: true });
+        return editedUser;
+      }
+      async deleteUser(userID): Promise<any> {
+        const deletedUser = await this.userModel
+          .findByIdAndRemove(userID);
+        return deletedUser;
+      }
+}
