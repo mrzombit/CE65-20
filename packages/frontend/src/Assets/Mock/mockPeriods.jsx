3ba29a6c4a09ce903bdf50d7e997f86482361e3f
mrzombit <54615665+mrzombit@users.noreply.github.com>
2023-02-02 03:33:11 +0700

    Add Period Mockdata

---
+++ packages/frontend/src/Assets/Mock/mockPeriods.jsx
@@ -0,0 +1,32 @@
+export default Periods = [
+    {
+        id: 1,
+        name: "monthly",
+        days: 30,
+        months: 1,
+    },
+    {
+        id: 2,
+        name: "quarterly",
+        days: 90,
+        months: 3,
+    },
+    {
+        id: 3,
+        name: "half year",
+        days: 180,
+        months: 6,
+    },
+    {
+        id: 4,
+        name: "yearly",
+        days: 360,
+        months: 12,
+    },
+    {
+        id: 5,
+        name: "weekly",
+        days: 7,
+        months: 0,
+    },
+]
