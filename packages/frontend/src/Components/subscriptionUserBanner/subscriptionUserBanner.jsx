b8bd00204ffa0761dec66f807c38e025838215fa
Chotika T <54322772+G-nap@users.noreply.github.com>
2023-02-03 21:07:16 +0700

    update subcription page

--- packages/frontend/src/Components/subscriptionUserBanner/subscriptionUserBanner.jsx
+++ packages/frontend/src/Components/subscriptionUserBanner/subscriptionUserBanner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./subscriptionUserBanner.css";
+import { SiMastercard } from "react-icons/si";
 
 function subscriptionUserBanner() {
   return (
@@ -11,9 +12,20 @@ function subscriptionUserBanner() {
       <div className="t-box2">
         <div>
           <div>Payment</div>
-          <div></div>
+          <div className="master-card-text">
+            <div>
+              <div>
+                <SiMastercard />
+                Mastercard ending in 7317
+              </div>
+              <div>Expires: 03/2026</div>
+            </div>
+          </div>
           <div>Next Billing is on 10 Oct 2022</div>
-          <div>Update</div>
+          <a href="">
+            {" "}
+            <div>Update</div>
+          </a>
         </div>
       </div>
     </div>
